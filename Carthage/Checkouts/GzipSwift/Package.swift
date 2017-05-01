9ebb3509eef37ce37645a93986ddcafb9b93cfbb
mb <mb@nesium.com>
2017-05-01 18:02:59 -0500

    removed Carthage/Checkouts

--- Carthage/Checkouts/GzipSwift/Package.swift
+++
@@ -1,6 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Gzip",
-    dependencies: [.Package(url: "https://github.com/1024jp/zlib.git", versions: Version(0,0,0)..<Version(1,0,0))]
-)
