a02da36e0aada7c6a862ed1e33de6c087b11aa34
mb <mb@nesium.com>
2018-02-13 01:23:08 +0100

    fixed dependencies

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     .library(name: "NSMWebservice", type: .dynamic, targets: ["NSMWebservice"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/groue/GRDB.swift.git", from: "2.8.0"),
     .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.1.2")
   ],
   targets: [
