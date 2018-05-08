5c550774550ebe81f902e0ef9817dbe1019bb7f3
mb <mb@nesium.com>
2018-05-08 14:29:01 +0200

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.1.2"), 
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("e14c1a3f423196c443fef25f5d306bc9106cd557"))
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .branch("master"))
   ],
   targets: [
     .target(name: "NSMWebservice", path: "NSMWebservice" )
